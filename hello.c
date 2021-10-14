229fc596c6befe33736390b32a64ba18b5e730b4
Sheethal03 <90698417+Sheethal03@users.noreply.github.com>
2021-10-14 23:53:46 +0530

    created c++file
    this file exist products of two numbers

---
+++ hello.c
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+   //c++ program to find product of two numbers
+       int x,y,product;
+       product=2*2
+       x=2;
+       y=2;
+
+  //product=x*y;
+      cout<<"product of two number is"<<product;
+  return 0;
+}
